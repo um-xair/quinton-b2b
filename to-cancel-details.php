e554323816c64da90f56bea534bab95d33be96a4
Alyssa <natasyahebat@gmail.com>
2024-12-19 10:40:39 +0800

    19-12-24

---
+++ to-cancel-details.php
@@ -0,0 +1,155 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+    <script src="https://cdn.tailwindcss.com"></script>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" rel="stylesheet">
+    <style>
+        body {
+            font-family: 'Poppins', sans-serif;
+        }
+    </style>
+</head>
+<body class="bg-[#F2F2F2] overflow-auto mb-[200px]">
+
+    <div class="flex items-center px-6 py-2 bg-[#F3BF01] h-32">
+        <a href="account.php">
+            <div class="bg-[#181C14] rounded-full p-2">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="white" viewBox="0 0 24 24" width="20px" height="20px">
+                    <path d="M15.41 16.59L10.83 12l4.58-4.59L14 6l-6 6 6 6z" />
+                </svg>
+            </div>
+        </a>
+        <h1 class="flex-1 text-center text-xl font-semibold text-[#181C14]">Cancellation Details</h1>
+    </div>
+
+    <div class="mt-6 px-4">
+        <div class="bg-white rounded-[20px]">
+            <div class="flex items-center justify-between bg-[#F3BF01] p-4 rounded-t-[20px]">
+                <div class="flex items-center">
+                    <label for="defaultAddress" class="text-md">Cancellation Details</label>
+                </div>
+            </div>
+            <div class="mt-4 px-4 pb-4">
+                <div class="flex justify-between items-center">
+                    <p class="text-[#181C14] text-md">Requested by</p>
+                    <p class="text-[#181C14] text-md">Buyer</p>
+                </div>
+                <div class="flex justify-between items-center mt-2">
+                    <p class="text-[#181C14] text-md">Requested at</p>
+                    <p class="text-[#181C14] text-md">12-12-2024 11:18AM</p>
+                </div>
+                <div class="flex justify-between items-center mt-2">
+                    <p class="text-[#181C14] text-md">Reason</p>
+                    <p class="text-[#181C14] text-md">don’t want to buy anymore</p>
+                </div>
+                <div class="flex justify-between items-center mt-2">
+                    <p class="text-[#181C14] text-md">Payment Method</p>
+                    <p class="text-[#181C14] text-md">Unpaid</p>
+                </div>
+            </div>
+
+        </div>
+    </div>
+
+    <div class="mt-6 px-4">
+        <div class="bg-white rounded-[20px]">
+            <div class="flex items-center justify-between bg-[#F3BF01] p-4 rounded-t-[20px]">
+                <div class="flex items-center">
+                    <label for="defaultAddress" class="text-md">Refund Details</label>
+                </div>
+            </div>
+            <div class="mt-4 px-4 pb-4">
+                <div class="flex justify-between items-center">
+                    <p class="text-[#181C14] text-md">Refund amount</p>
+                    <p class="text-[#181C14] text-md">RM 1000.00</p>
+                </div>
+                <div class="flex justify-between items-center mt-2">
+                    <p class="text-[#181C14] text-md">Refund to</p>
+                    <p class="text-[#181C14] text-md">Buyer account</p>
+                </div>
+                <div class="flex justify-between items-center mt-2">
+                    <p class="text-[#181C14] text-md">Requested by</p>
+                    <p class="text-[#181C14] text-md">Buyer</p>
+                </div>
+                <div class="flex justify-between items-center mt-2">
+                    <p class="text-[#181C14] text-md">Requested at</p>
+                    <p class="text-[#181C14] text-md">12-12-2024 11:18AM</p>
+                </div>
+            </div>
+
+        </div>
+    </div>
+
+    <div class="mt-6 px-4">
+        <div class="bg-white rounded-[20px] px-4 py-6">
+            <div class="flex items-center space-x-6">
+                <div class="w-40 h-24 bg-[#F2F2F2] rounded-[10px] p-2 flex items-center justify-center">
+                    <img src="assets/Nior Creamy White.webp" alt="Stroller" class="w-full h-full object-cover">
+                </div>
+    
+                <div class="flex flex-col space-y-2 w-full">
+                    <h2 class="text-lg font-bold text-[#181C14]">Nior Stroller</h2>
+                    <p class="text-sm text-gray-500">Variation: Black</p>
+                    <div class="flex justify-between items-center mt-4">
+                        <p class="text-lg font-semibold text-[#181C14]">RM1499.00</p>
+                        <span class="text-lg font-bold text-[#181C14]">x10</span>
+                    </div>
+                </div>
+            </div>
+    
+            <hr class="my-4 border-gray-300">
+    
+            <div class="flex justify-between items-center">
+                <p class="text-md font-semibold text-[#181C14]">Total 10 item(s)</p>
+                <p class="text-md font-semibold text-[#181C14]">RM14990.00</p>
+            </div>
+        </div>
+    </div>
+
+    <div class="mt-6 px-4">
+        <div class="bg-white rounded-[20px]">
+            <div class="flex items-center justify-between bg-[#F3BF01] p-4 rounded-t-[20px]">
+                <div class="flex items-center">
+                    <p class="text-md">Support Center</p>
+                </div>
+            </div>
+            <div class="mt-4 px-4 pb-6 flex justify-between items-center">
+                <div class="flex items-center space-x-2">
+                    <i class="fa-regular fa-comment text-[#181C14] text-xl"></i>
+                    <span class="text-md text-[#181C14]">Contact Sales Person</span>
+                </div>
+                <i class="fas fa-chevron-right text-[#181C14] text-xl"></i>
+            </div>
+        </div>
+    </div>
+
+    <div class="mt-6 px-4">
+        <div class="bg-white rounded-[20px]">
+            <div class="flex items-center justify-between bg-[#F3BF01] p-4 rounded-t-[20px]">
+                <div class="flex items-center">
+                    <label for="defaultAddress" class="text-md">Order ID</label>
+                </div>
+            </div>
+            <div class="mt-4 p-4">
+                <div class="flex items-center space-x-2 mb-4">
+                    <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#181C14">
+                        <path d="M360-240q-33 0-56.5-23.5T280-320v-480q0-33 23.5-56.5T360-880h360q33 0 56.5 23.5T800-800v480q0 33-23.5 56.5T720-240H360Zm0-80h360v-480H360v480ZM200-80q-33 0-56.5-23.5T120-160v-560h80v560h440v80H200Zm160-240v-480 480Z"/>
+                    </svg>
+                    <p class="text-[#181C14] text-md">98989818373-P</p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="fixed bottom-10 left-4 right-4 bg-[#F3BF01] py-4 px-4 rounded-full z-[999]">
+        <div class="flex justify-end gap-2">
+            <button class="w-1/2 bg-[#181C14] text-white py-3 rounded-full">Buy Again</button>
+        </div>
+    </div>
+
+</body>
+</html>
