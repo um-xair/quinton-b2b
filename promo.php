d0580537d0685e968055c16ad049786f83328230
Alyssa <natasyahebat@gmail.com>
2024-12-21 09:49:41 +0800

    21-12-24

--- promo.php
+++ promo.php
@@ -37,34 +37,44 @@
             <div>
                 <div class="relative bg-[#F2F2F2] rounded-[20px] p-4">
                     <img src="assets/Nior Creamy White.webp" alt="Product" class="w-full h-full object-cover rounded-[20px]">
-                    <div class="absolute top-2 right-2 bg-white p-2 rounded-full">
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="#F3BF01" width="20px" height="20px" viewBox="0 0 24 24" stroke="none">
-                            <path d="m12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.09C13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5c0 3.78-3.4 6.86-8.55 11.54L12 21.35z"/>
-                        </svg>
+                    <div class="absolute top-2 right-2 bg-white rounded-full w-10 h-10 flex items-center justify-center">
+                        <i class="fa-regular fa-heart text-[#F3BF01] text-xl cursor-pointer" onclick="toggleHeart(this)"></i>
                     </div>
                 </div>
-                <div class="mt-2">
+                <div class="mt-2 text-center">
                     <h3 class="text-sm text-[#181C14]">Nior Stroller</h3>
                     <p class="text-lg text-[#181C14] font-semibold">RM1499.00</p>
                 </div>
             </div>
-    
             <div>
                 <div class="relative bg-[#F2F2F2] rounded-[20px] p-4">
                     <img src="assets/Nior Creamy White.webp" alt="Product" class="w-full h-full object-cover rounded-[20px]">
-                    <div class="absolute top-2 right-2 bg-white p-2 rounded-full">
-                        <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#F3BF01">
-                            <path d="m480-120-58-52q-101-91-167-157T150-447.5Q111-500 95.5-544T80-634q0-94 63-157t157-63q52 0 99 22t81 62q34-40 81-62t99-22q94 0 157 63t63 157q0 46-15.5 90T810-447.5Q771-395 705-329T538-172l-58 52Zm0-108q96-86 158-147.5t98-107q36-45.5 50-81t14-70.5q0-60-40-100t-100-40q-47 0-87 26.5T518-680h-76q-15-41-55-67.5T300-774q-60 0-100 40t-40 100q0 35 14 70.5t50 81q36 45.5 98 107T480-228Zm0-273Z"/>
-                        </svg>
+                    <div class="absolute top-2 right-2 bg-white rounded-full w-10 h-10 flex items-center justify-center">
+                        <i class="fa-regular fa-heart text-[#F3BF01] text-xl cursor-pointer" onclick="toggleHeart(this)"></i>
                     </div>
                 </div>
-                <div class="mt-2">
+                <div class="mt-2 text-center">
                     <h3 class="text-sm text-[#181C14]">Nior Stroller</h3>
                     <p class="text-lg text-[#181C14] font-semibold">RM1499.00</p>
                 </div>
             </div>
         </div>
     </div>
+
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" rel="stylesheet">
+
+    <script>
+        function toggleHeart(iconElement) {
+            if (iconElement.classList.contains('fa-regular')) {
+                iconElement.classList.remove('fa-regular');
+                iconElement.classList.add('fa-solid');
+            } else {
+                iconElement.classList.remove('fa-solid');
+                iconElement.classList.add('fa-regular');
+            }
+        }
+    </script>
+
   
 </body>
 </html>
